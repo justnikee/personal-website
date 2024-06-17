c86bb5d16f764c897cc8de295206f25314e36e6c
Nikeeyyy <hk5561111212@gmail.com>
2024-06-18 01:14:53 +0530

    res changes

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
 import nikee from '../../public/nike.png'
+import apiImage from '../../public/api.jpg'
+import Link from 'next/link'
 
 type Props = {}
 
 const Projects = (props: Props) => {
   return (
     <div id="projects" className='bg-black pb-20 w-full'>
-    <div className='bg-pink-300 m-auto rounded-xl max-w-[95%]  pt-32 md:pt-24 px-12 py-12'>
+    <div className='bg-pink-300 m-auto rounded-xl max-w-[95%]  pt-32 md:pt-24 px-4 sm:px-12 py-12'>
       <div className='relative sm:h-24 h-64'>
       <h2 className='big-cursor stager-text1 text-[#564cf8] font-["damn"] text-[130px] leading-[117px] text-center absolute top-0 right-0 bottom-0 left-0'>
         SOME PROJECTS
@@ -18,8 +20,8 @@ const Projects = (props: Props) => {
       </div>
       <div className='sm:grid sm:grid-cols-2 sm:gap-8 sm:pt-24 flex flex-col gap-8'>
         <div>
-          <div className='rounded-xl p-4 bg-[#fefefe]'>
-            <Image alt='nike' src={nikee}/>
+          <div className='rounded-xl p-4 bg-[#fefefe] max-h-80'>
+            <Image className='h-full' alt='nike' src={nikee}/>
           </div>
           <div className='flex flex-row gap-3 mt-5 flex-wrap'>
             <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Web Development</span>
@@ -27,19 +29,25 @@ const Projects = (props: Props) => {
             <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Web Design</span>
             <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>E-Commerce</span>
           </div>
+          <div className='flex justify-between items-center'>
           <h3 className='text-black font-["damn"] text-[40px] leading-10 pt-4'>Nike Clone</h3>
+          <Link href="https://github.com/justnikee/fuzzy-potato" target='_blank' className='bg-[#BEF941] text-sm px-6 py-3 rounded-3xl mt-5 block w-fit uppercase font-bold'>View Code</Link>
+          </div>
         </div>
         <div>
-          <div className='rounded-xl p-4 bg-[#fefefe]'>
-            <Image alt='nike' src={nikee}/>
+          <div className='rounded-xl p-4 bg-[#fefefe] max-h-80 overflow-hidden'>
+            <Image className='h-full object-cover' alt='nike' src={apiImage}/>
           </div>
           <div className='flex flex-row gap-3 mt-5 flex-wrap'>
             <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Web Development</span>
-            <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Frontend</span>
-            <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Web Design</span>
-            <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>E-Commerce</span>
+            <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Backend</span>
+            <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>NodeJs</span>
+            <span className='text-sm uppercase text-white bg-[#564cf8] border-1 border-white rounded-xl px-2 py-1'>Shop API</span>
           </div>
-          <h3 className='text-black font-["damn"] text-[40px] leading-10 pt-4'>Nike Clone</h3>
+          <div className='flex justify-between items-center'>
+          <h3 className='text-black font-["damn"] text-[40px] leading-10 pt-4'>Backend Store API</h3>
+          <Link href="https://github.com/justnikee/Backend" target='_blank' className='bg-[#BEF941] text-sm px-6 py-3 rounded-3xl mt-5 block w-fit uppercase font-bold'>View Code</Link>
+          </div>
         </div>
       </div>
     </div>
--- app/components/socials.tsx
+++ app/components/socials.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const SocialSection = () => {
     return(
         <div id="socials" className="w-full bg-black pb-12">
-        <div className="flex justify-between w-full max-w-[95%] m-auto rounded-xl bg-[#564cf8] p-8">
-            <div className="flex w-fit gap-8">
-            <Link className="big-cursor text-2xl leading-normal" href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+        <div className="flex flex-col sm:flex-row text-center sm:text-left gap-4 sm:gap-0 justify-between w-full max-w-[95%] m-auto rounded-xl bg-[#564cf8] p-8">
+            <div className="flex flex-wrap justify-center sm:justify-normal gap-4 sm:flex-nowrap w-fit sm:sgap-8">
+            <Link className="big-cursor text-2xl leading-normal" href="https://x.com/Nxkeeyy" target="_blank" rel="noopener noreferrer">
                 <span className='stager-text1 uppercase text-[#BEF941] font-["damn"] text-4xl'>X</span>
                 </Link>
                 <Link className="big-cursor text-2xl leading-normal" href="https://www.instagram.com/nikxxyyy_" target="_blank" rel="noopener noreferrer">
@@ -15,9 +15,12 @@ const SocialSection = () => {
                 <Link className="big-cursor text-2xl leading-normal" href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
                 <span className='stager-text1 uppercase text-[#BEF941] font-["damn"] text-4xl '>LinkedIn</span>
                 </Link>
+                <Link className="big-cursor text-2xl leading-normal" href="https://github.com/justnikee" target="_blank" rel="noopener noreferrer">
+                <span className='stager-text1 uppercase text-[#BEF941] font-["damn"] text-4xl '>Github</span>
+                </Link>
             </div>
             <div className="flex">
-        <span className='stager-text1 uppercase text-[#BEF941] font-["damn"] text-4xl '>Crafted with Love @750°F - Chandigarh INDIA</span>
+        <span className='stager-text1 uppercase text-[#BEF941] font-["damn"] text-4xl '>Crafted with 💖 - Chandigarh INDIA</span>
         </div>
         </div>
   
